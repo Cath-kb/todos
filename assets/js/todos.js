97470b59e241b8f9bff42a71454f390eb672a1d4
Cath_kb <catherine.nikolaev@gmail.com>
2015-07-30 21:22:58 +0300

    js draft

---
+++ assets/js/todos.js
@@ -0,0 +1,35 @@
+//"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+
+//array of todos
+var todos = [];
+
+//todo obj (item or todos array)
+function todo() {
+	var id, content, status;
+};
+
+/*filters*/
+var filterShowAll, filterShowActive, filterShowCompleted, clearColmpleted, todoCount;
+
+//show all todos
+
+//show active todos
+
+//show completed
+
+
+
+//operations:
+
+//add todo after lost focus and not empty or after enter key
+
+//clear todo - remove from html-view (but not from array)
+
+//clear completed todos
+
+//edit todo - after double click start and after enter finish. if focus lost, no changes
+
+//add/remove checked mark for all todos
+
+//add/remove checked mark for current todo
+
