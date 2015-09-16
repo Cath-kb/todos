88ceb6a5030a579299f54b641d16d4e060826f31
Cath_kb <catherine.nikolaev@gmail.com>
2015-09-16 08:47:04 +0300

    add statuses, collection & rendering

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,50 @@
 //"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
-//array of todos
-var todos = [];
+var todoStatusList = ['active', 'completed', 'removed'];
 
-//todo obj (item or todos array)
-function todo() {
-	var id, content, status;
+//array of todos
+var todos = [
+	{
+		id: "0",
+		content: "todo first",
+		status: todoStatusList[1]
+	},
+	{
+		id: "1",
+		content: "todo second",
+		status: todoStatusList[0]
+	},
+	{
+		id: "2",
+		content: "todo third",
+		status: todoStatusList[2]
+	}
+];
+
+console.log(todos);
+
+//insert todos into DOM
+var insertTodoIntoDOM = function(todos) {
+	var todoList = '';
+
+	for (var i in todos) {
+		var todoTemplate = '<li data-id="'+todos[i]["id"]+'" class="todo-list-item">'+
+			'<div class="view">'+
+			'<input class="toggle" type="checkbox">'+
+			'<label class="view-label-text">'+todos[i]["content"]+'</label>'+
+			'<button class="remove"></button>'+
+			'</div>'+
+			'</li>';
+		todoList += todoTemplate;
+	}
+	var element = document.getElementsByClassName("todo-list")[0];
+	element.innerHTML = todoList;
+	console.log(element);
 };
 
+window.onload = function(e){
+insertTodoIntoDOM(todos);
+};
 /*filters*/
 var filterShowAll, filterShowActive, filterShowCompleted, clearColmpleted, todoCount;
 
